652b0668ae786087735e6d8772a5bf87afe9f0be
Kyuho-Han <rodius93@naver.com>
2021-06-03 19:59:41 +0900

    06.03

--- router/mainRouter.js
+++ router/mainRouter.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const router = express.Router();
+const db = require('../model/db');
 
 
 router.get("/", function(req,res){
@@ -18,5 +19,31 @@ router.post("/postapi", function(req, res){
     res.send('POST API')
 })
 
+router.get("/data/create",function(req,res){
+    let user_id = parseInt(Math.random() * 10000);
+    db.users.create({user_id:user_id}).then(function(result){
+        res.send({success:200})
+    })
+})
+
+router.get("/data/read", function(req,res){
+    db.users.findAll().then(function(result){
+        res.send({success:200, data:result})
+    })
+})
+
+router.post("/data/update", function(req,res){
+    let target_id = req.body.target_id;
+    db.users.update({user_id:9999},{where:{user_id:target_id}}).then(function(result){
+        res.send({success:200})
+    })
+})
+
+router.post("/data/delete",function(req,res){
+    let target_id = req.body.target_id;
+    db.users.destroy({where:{user_id:target_id}}).then(function(result){
+        res.send({success:200})
+    })
+})
 
 module.exports = router //라우터 변수
