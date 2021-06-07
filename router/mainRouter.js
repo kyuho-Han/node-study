a0d186013fd1966176321ad062ef9338f086cf50
Kyuho-Han <rodius93@naver.com>
2021-06-07 19:23:53 +0900

    06.07

--- router/mainRouter.js
+++ router/mainRouter.js
@@ -1,11 +1,34 @@
-const express = require('express')
+const express = require('express');
 const router = express.Router();
 const db = require('../model/db');
 
 
 router.get("/", function(req,res){
-
    res.render('main',{title:"영화 리뷰 사이트"})
+})
+
+router.post("/review/create", function(req,res){
+    let movie_id = req.body.movie_id;
+    let review = req.body.review;
+
+    if(movie_id == '' || movie_id == 0){
+        res.send({success:400})
+    }else{
+        db.reviews.create({
+            movie_id: movie_id,
+            review:review
+        }).then(function(result){
+            res.send({success:200})
+        })
+    }
+})
+
+router.get("/review/read", function(req,res){
+    let movie_id = req.query.movie_id;
+
+    db.reviews.findAll({where:{movie_id:movie_id}}).then(function(result){
+        res.send({success:200, data:result})
+    })
 })
 
 //회사소개 페이지
@@ -46,20 +69,6 @@ router.post("/data/delete",function(req,res){
     })
 })
 
-router.post("/review/create", function(req,res){
-    let movie_id = req.body.movie_id;
-    let review = req.body.review;
 
-    if(movie_id == '' || movie_id == 0){
-        res.send({success:400})
-    }else{
-        db.reviews.create({
-            movie_id: movie_id,
-            review:review
-        }).then(function(result){
-            res.send({success:200})
-        })
-    }
-})
 
 module.exports = router //라우터 변수
